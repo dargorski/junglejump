1d5078cee9e68f4a364567a5c69d2363bd2f5a59
Dariusz Gorski <dariuszgoorski@gmail.com>
2024-01-25 18:59:50 +0100

    added collectible items

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,10 +1,20 @@
 extends CharacterBody2D
+signal life_changed
+signal died
 @export var gravity = 750
 @export var run_speed = 150
 @export var jump_speed = -300
 
 enum STATE {IDLE, RUN, JUMP, HURT, DEAD}
 var state = STATE.IDLE
+
+var life = 3: set = set_life
+
+func set_life(value):
+	life = value
+	life_changed.emit(life)
+	if life <= 0:
+		change_state(STATE.DEAD)
 
 func _ready():
 	change_state(STATE.IDLE)
@@ -28,12 +38,20 @@ func change_state(new_state: STATE):
 			$AnimationPlayer.play("run")
 		STATE.HURT:
 			$AnimationPlayer.play("hurt")
+			velocity.y = -200
+			velocity.x = -100 * sign(velocity.x)
+			life -= 1
+			await get_tree().create_timer(0.5).timeout
+			change_state(STATE.IDLE)
 		STATE.JUMP:
 			$AnimationPlayer.play("jump_up")
 		STATE.DEAD:
+			died.emit()
 			hide()
 
 func get_input():
+	if state == STATE.HURT:
+		return
 	var right = Input.is_action_pressed("right")
 	var left = Input.is_action_pressed("left")
 	var jump = Input.is_action_just_pressed("jump")
@@ -57,7 +75,12 @@ func get_input():
 	if state in [STATE.IDLE, STATE.RUN] and !is_on_floor():
 		change_state(STATE.JUMP)
 
+func hurt():
+	if state != STATE.HURT:
+		change_state(STATE.HURT)
+
 func reset(_position):
 	position = _position
+	life = 3
 	show()
 	change_state(STATE.IDLE)
